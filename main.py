c98bfcf7c8343f4cf3c4f5d534511e93a3b862df
Subha2001 <das.subhajittribeni@gmail.com>
2025-07-11 19:21:48 +0530

    Set number of genres in training configuration to 19 for proper padding

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from utils import *
 def train():
     export_root = setup_train(args)
     train_loader, val_loader, test_loader = dataloader_factory(args)
+    args.num_genres = 19  # 18 genres + 1 for padding (0)
     model = model_factory(args)
     trainer = trainer_factory(args, model, train_loader, val_loader, test_loader, export_root)
     trainer.train()
