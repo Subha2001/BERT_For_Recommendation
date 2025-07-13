61ba3b206df14a1526e08949b4adc9564d180fdf
Subha2001 <das.subhajittribeni@gmail.com>
2025-07-13 10:07:03 +0530

    Updated Epoch to 100

--- templates.py
+++ templates.py
@@ -33,7 +33,7 @@ def set_template(args):
         args.enable_lr_schedule = True
         args.decay_step = 25
         args.gamma = 1.0
-        args.num_epochs = 200 if args.dataset_code == 'ml-1m' else 200
+        args.num_epochs = 100 if args.dataset_code == 'ml-1m' else 200
         args.metric_ks = [1, 5, 10, 20, 50, 100]
         args.best_metric = 'NDCG@10'
 
@@ -72,7 +72,7 @@ def set_template(args):
         args.lr = 1e-3
         args.enable_lr_schedule = False
         args.weight_decay = 0.00
-        args.num_epochs = 200 if args.dataset_code == 'ml-1m' else 200
+        args.num_epochs = 100 if args.dataset_code == 'ml-1m' else 200
         args.metric_ks = [1, 5, 10, 20, 50, 100]
         args.best_metric = 'NDCG@10'
 
@@ -108,7 +108,7 @@ def set_template(args):
         args.lr = 1e-3
         args.enable_lr_schedule = False
         args.weight_decay = 0.01
-        args.num_epochs = 200 if args.dataset_code == 'ml-1m' else 200
+        args.num_epochs = 100 if args.dataset_code == 'ml-1m' else 200
         args.metric_ks = [1, 5, 10, 20, 50, 100]
         args.best_metric = 'NDCG@10'
         args.total_anneal_steps = 3000 if args.dataset_code == 'ml-1m' else 20000
@@ -146,7 +146,7 @@ def set_template(args):
         args.lr = 1e-3
         args.enable_lr_schedule = False
         args.weight_decay = 0.01
-        args.num_epochs = 200 if args.dataset_code == 'ml-1m' else 200
+        args.num_epochs = 100 if args.dataset_code == 'ml-1m' else 200
         args.metric_ks = [1, 5, 10, 20, 50, 100]
         args.best_metric = 'NDCG@100'
         args.find_best_beta = False
