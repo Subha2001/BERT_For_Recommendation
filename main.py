262b20473252278e1c288442ec6618737cefb7e0
Subha2001 <das.subhajittribeni@gmail.com>
2025-07-13 23:27:03 +0530

    Made changes for requirement

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from utils import *
 
 def train():
     export_root = setup_train(args)
+    ############################################################################
+    # Ensure export_root exists
+    ############################################################################
     # Create dataset and print genre diversity
     from datasets import dataset_factory
     dataset = dataset_factory(args)
@@ -25,6 +28,9 @@ def train():
     genre_counts = {}
     for g in genre_ids:
         genre_counts[g] = genre_counts.get(g, 0) + 1
+    ######################################################################
+    # Newly added code ends here
+    ######################################################################
     train_loader, val_loader, test_loader = dataloader_factory(args)
     model = model_factory(args)
     trainer = trainer_factory(args, model, train_loader, val_loader, test_loader, export_root)
